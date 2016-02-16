ab66059811fb989608c1bf2fd728914c748eef86
Nathan Benson <Benson@Nathans-MacBook-Pro.local>
2016-02-16 17:01:02 -0600

    i did stuff

--- AV/Development/NathanArrayScramble.js
+++ AV/Development/NathanArrayScramble.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     JSAV.init();
 
-                  var av = new JSAV("container");
+                  var av = new JSAV("NathanArrayScramble");
                   var theArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
                   var arr = av.ds.array(theArray, {indexed: true});
                   av.umsg("Start array");
